5ddf61330e2d78be5650be44263770e7a85ffb65
Pam Stadler <43047254+tex2003@users.noreply.github.com>
2019-03-13 14:56:38 -0500

    Adding Primary Key and Count

--- MySQL/disease_db.sql
+++ MySQL/disease_db.sql
@@ -2,13 +2,14 @@ DROP DATABASE IF EXISTS disease_db;
 CREATE DATABASE disease_db;
 USE disease_db;
 CREATE TABLE disease_data (
+  ID int NOT NULL AUTO_INCREMENT PRIMARY KEY,
   Disease VARCHAR(50) NOT NULL,
   Fatalities INT,
   State VARCHAR(50),
   City VARCHAR(50),
   PeriodStartDate DATE,
   PeriodEndDate DATE,
-  Count Int
+  ValueCount Int
 );
 
 
