6bd348911771cddeadaeea00cddd2d62f8bd68c4
Aaron Wierengo <awierengo@utexas.edu>
2019-03-18 18:37:54 -0500

    fixed basic route

--- web_app/app.py
+++ web_app/app.py
@@ -48,7 +48,7 @@ def home():
 def basic():
     # Query grouped by Disease & Year
     results = db.session.query(Disease_Data.Disease,func.sum(Disease_Data.CountValue),func.sum(Disease_Data.Fatalities),Disease_Data.Year).\
-    group_by(Disease_Data.State).all()
+    group_by(Disease_Data.Disease).group_by(Disease_Data.Year).all()
 
     disease_data = []
     for result in results:
@@ -117,54 +117,6 @@ def year_disease(year):
         disease_data.append(disease_dict)
 
     return jsonify(disease_data)
-# @app.route("/api/<year>")
-# def api(year):
-#         sel = [
-#         Disease_Data.Disease,
-#         Disease_Data.State,
-#         Disease_Data.PeriodEndDate,
-#         Disease_Data.Fatalities,
-#         Disease_Data.CountValue
-#     ]
-#     # Need to make query select specific year
-#     # results = db.session.query(*sel).filter(extract('year',Disease_Data.PeriodEndDate) == year).all()
-#     # results = db.session.query(*sel).filter(Disease_Data.Year == year).all()
-
-#     disease_data = []
-#     for result in results:
-#         disease_dict = {}
-#         disease_dict["Disease"] = result[0]
-#         disease_dict["State"] = result[1]
-#         disease_dict["PeriodEnd"] = result[2]
-#         disease_dict["Fatalities"] = result[3]
-#         disease_dict["Count"] = result[4]
-#         disease_data.append(disease_dict)
-
-#     return jsonify(disease_data)
-
-# @app.route("/test")
-# def test():
-#     sel = [
-#         Disease.Disease,
-#         Disease.State,
-#         Disease.Year,
-#         Disease.Fatalities,
-#         Disease.CountValue
-#     ]
-
-#     results = db.session.query(*sel).all()
-
-#     disease_data = [{
-#         "disease":result[0],
-#         "state":result[1],
-#         "year":result[2],
-#         "fatalities":result[3],
-#         "count":result[4]
-#     } for result in results]
-
-
-#     return jsonify(disease_data)
-
 
 if __name__ == "__main__":
     app.run()
