d7fe8b65d3c75e0fc417a522b891c7889a5b8973
masonwalters72 <masonwalters@utexas.edu>
2019-03-19 01:57:02 -0500

    Commiting updated file structure, CSS, and app.js file

--- web_app/static/js/app.js
+++ web_app/static/js/app.js
@@ -1,212 +1,182 @@
+// Define SVG area dimensions
 var svgWidth = 960;
 var svgHeight = 500;
 
+// Define the chart's margins as an object
 var margin = {
-  top: 20,
-  right: 40,
-  bottom: 80,
-  left: 100
+  top: 60,
+  right: 60,
+  bottom: 60,
+  left: 60
 };
 
-var width = svgWidth - margin.left - margin.right;
-var height = svgHeight - margin.top - margin.bottom;
+// Define dimensions of the chart area
+var chartWidth = svgWidth - margin.left - margin.right;
+var chartHeight = svgHeight - margin.top - margin.bottom;
 
-// Create an SVG wrapper, append an SVG group that will hold our chart,
-// and shift the latter by left and top margins.
-var svg = d3
-  .select(".chart")
+// Select body, append SVG area to it, and set its dimensions
+var svg = d3.select("#chart_1")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
 
-// Append an SVG group
+// Append a group area, then set its margins
 var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-// Initial Params
-var chosenXAxis = "disease";
+  
+// Configure a parseTime function which will return a new Date object from a string
+var basicURL = '/basic'
+// Load data from forcepoints.csv
+d3.json(basicURL, function(error, basicData) {
 
-// function used for updating x-scale var upon click on axis label
-function xScale(disease, chosenXAxis) {
-  // create scales
-  var xLinearScale = d3.scaleLinear()
-    .domain([d3.min(disease, d => d[chosenXAxis]) * 0.8,
-      d3.max(disease, d => d[chosenXAxis]) * 1.2
-    ])
-    .range([0, width]);
-
-  return xLinearScale;
-
-}
-
-// function used for updating xAxis var upon click on axis label
-function renderAxes(newXScale, xAxis) {
-  var bottomAxis = d3.axisBottom(newXScale);
-
-  xAxis.transition()
-    .duration(1000)
-    .call(bottomAxis);
-
-  return xAxis;
-}
-
-// function used for updating circles group with a transition to
-// new circles
-function renderCircles(circlesGroup, newXScale, chosenXaxis) {
-
-  circlesGroup.transition()
-    .duration(1000)
-    .attr("cx", d => newXScale(d[chosenXAxis]));
-
-  return circlesGroup;
-}
-
-// function used for updating circles group with new tooltip
-function updateToolTip(chosenXAxis, circlesGroup) {
-
-  if (chosenXAxis === "diseaseCount") {
-    var label = "Hair Length:";
-  }
-  else {
-    var label = "# of Albums:";
-  }
-
-  var toolTip = d3.tip()
-    .attr("class", "tooltip")
-    .offset([80, -60])
-    .html(function(d) {
-      return (`${d.State}<br>${label} ${d[chosenXAxis]}`);
-    });
-
-  circlesGroup.call(toolTip);
-
-  circlesGroup.on("mouseover", function(data) {
-    toolTip.show(data);
-  })
-    // onmouseout event
-    .on("mouseout", function(data, index) {
-      toolTip.hide(data);
-    });
-
-  return circlesGroup;
-}
-
-// Retrieve data from the CSV file and execute everything below
-d3.csv("./../data", function(err, disease) {
-  if (err) throw err;
-
-  // parse data
-  disease.forEach(function(data) {
-    data.diseaseCount = +data.diseaseCount;
-    data.num_hits = +data.num_hits;
-    data.num_albums = +data.num_albums;
+  var dip = basicData.filter(function(d){
+    return d.Disease === 'Diphtheria'
+  });
+  
+  var measles = basicData.filter(function(d){
+    return d.Disease === 'Measles'
+  });
+  
+  var mumps = basicData.filter(function(d){
+    return d.Disease === 'Mumps'
+  });
+  
+  var pert = basicData.filter(function(d){
+    return d.Disease === 'Pertussis'
+  });
+  
+  var rub = basicData.filter(function(d){
+    return d.Disease === 'Rubella'
+  });
+  
+  var small = basicData.filter(function(d){
+    return d.Disease === 'Smallpox'
+  });
+  
+  var tube = basicData.filter(function(d){
+    return d.Disease === 'Tuberculosis'
+  });
+  
+  var typ = basicData.filter(function(d){
+    return d.Disease === 'Typhoid fever'
+  });
+  
+  var varic = basicData.filter(function(d){
+    return d.Disease === 'Varicella'
+  });
+  
+  var hepb = basicData.filter(function(d){
+    return d.Disease === 'Viral hepatitis type B'
+  });
+  
+  var hepa = basicData.filter(function(d){
+    return d.Disease === 'Viral hepatitis, type A'
   });
 
-  // xLinearScale function above csv import
-  var xLinearScale = xScale(disease, chosenXAxis);
+var allDiseases = ['dip', 'measles', 'mumps', 'pert', 'rub', 'small', 'tube', 'typ', 'varic', 'hepb', 'hepa']
+var accent = d3.scaleOrdinal(d3.schemePaired);
 
-  // Create y scale function
+  // Throw an error if one occurs
+  if (error) throw error;
+
+  // Print the basicData
+  console.log(basicData);
+
+  // Format the date and cast the force value to a number
+  basicData.forEach(function(data) {
+    data.Year = +data.Year;
+    data.Count = +data.Count;
+  });
+
+  // Configure a time scale
+  // d3.extent returns the an array containing the min and max values for the property specified
+  var xLinearScale = d3.scaleLinear()
+    .domain([d3.min(basicData, data => data.Year), d3.max(basicData, data => data.Year)])
+    .range([0, chartWidth]);
+
+  // Configure a linear scale with a range between the chartHeight and 0
   var yLinearScale = d3.scaleLinear()
-    .domain([0, d3.max(disease, d => d.num_hits)])
-    .range([height, 0]);
+    .domain([0, d3.max(basicData, data => data.Count)])
+    .range([chartHeight, 0]);
 
-  // Create initial axis functions
+  // Create two new functions passing the scales in as arguments
+  // These will be used to create the chart's axes
   var bottomAxis = d3.axisBottom(xLinearScale);
   var leftAxis = d3.axisLeft(yLinearScale);
 
-  // append x axis
-  var xAxis = chartGroup.append("g")
-    .classed("x-axis", true)
-    .attr("transform", `translate(0, ${height})`)
-    .call(bottomAxis);
+  // Configure a line function which will plot the x and y coordinates using our scales
+  var drawLine = d3.line()
+    .x(data => xLinearScale(data.Year))
+    .y(data => yLinearScale(data.Count));
 
-  // append y axis
+  // Append an SVG path and plot its points using the line function
+  chartGroup.append("path")
+    // The drawLine function returns the instructions for creating the line for basicData
+    .attr("d", drawLine(dip))
+    .classed("dip", true);
+
+  chartGroup.append("path")
+    // The drawLine function returns the instructions for creating the line for basicData
+    .attr("d", drawLine(measles))
+    .classed('measles', true)
+
+    chartGroup.append("path")
+    // The drawLine function returns the instructions for creating the line for basicData
+    .attr("d", drawLine(mumps))
+    .classed('mumps', true)
+
+    chartGroup.append("path")
+    // The drawLine function returns the instructions for creating the line for basicData
+    .attr("d", drawLine(pert))
+    .classed('pert', true)
+
+    chartGroup.append("path")
+    // The drawLine function returns the instructions for creating the line for basicData
+    .attr("d", drawLine(rub))
+    .classed('rub', true)
+
+    chartGroup.append("path")
+    // The drawLine function returns the instructions for creating the line for basicData
+    .attr("d", drawLine(small))
+    .classed('small', true)
+
+    chartGroup.append("tube")
+    // The drawLine function returns the instructions for creating the line for basicData
+    .attr("d", drawLine(measles))
+    .classed('tube', true)
+
+    chartGroup.append("path")
+    // The drawLine function returns the instructions for creating the line for basicData
+    .attr("d", drawLine(typ))
+    .classed('typ', true)
+
+    chartGroup.append("path")
+    // The drawLine function returns the instructions for creating the line for basicData
+    .attr("d", drawLine(varic))
+    .classed('varic', true)
+
+    chartGroup.append("path")
+    // The drawLine function returns the instructions for creating the line for basicData
+    .attr("d", drawLine(hepb))
+    .classed('hepb', true)
+
+    chartGroup.append("path")
+    // The drawLine function returns the instructions for creating the line for basicData
+    .attr("d", drawLine(hepa))
+    .classed('hepa', true)
+  
+
+  // Append an SVG group element to the chartGroup, create the left axis inside of it
   chartGroup.append("g")
+    .classed("axis", true)
     .call(leftAxis);
 
-  // append initial circles
-  var circlesGroup = chartGroup.selectAll("circle")
-    .data(disease)
-    .enter()
-    .append("circle")
-    .attr("cx", d => xLinearScale(d[chosenXAxis]))
-    .attr("cy", d => yLinearScale(d.num_hits))
-    .attr("r", 20)
-    .attr("fill", "pink")
-    .attr("opacity", ".5");
+  // Append an SVG group element to the chartGroup, create the bottom axis inside of it
+  // Translate the bottom axis to the bottom of the page
+  chartGroup.append("g")
+    .classed("axis", true)
+    .attr("transform", `translate(0, ${chartHeight})`)
+    .call(bottomAxis);
 
-  // Create group for  2 x- axis labels
-  var labelsGroup = chartGroup.append("g")
-    .attr("transform", `translate(${width / 2}, ${height + 20})`);
-
-  var hairLengthLabel = labelsGroup.append("text")
-    .attr("x", 0)
-    .attr("y", 20)
-    .attr("value", "diseaseCount") // value to grab for event listener
-    .classed("active", true)
-    .text("Hair Metal Ban Hair Length (inches)");
-
-  var albumsLabel = labelsGroup.append("text")
-    .attr("x", 0)
-    .attr("y", 40)
-    .attr("value", "num_albums") // value to grab for event listener
-    .classed("inactive", true)
-    .text("# of Albums Released");
-
-  // append y axis
-  chartGroup.append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 0 - margin.left)
-    .attr("x", 0 - (height / 2))
-    .attr("dy", "1em")
-    .classed("axis-text", true)
-    .text("Number of Billboard 500 Hits");
-
-  // updateToolTip function above csv import
-  var circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
-
-  // x axis labels event listener
-  labelsGroup.selectAll("text")
-    .on("click", function() {
-      // get value of selection
-      var value = d3.select(this).attr("value");
-      if (value !== chosenXAxis) {
-
-        // replaces chosenXaxis with value
-        chosenXAxis = value;
-
-        // console.log(chosenXAxis)
-
-        // functions here found above csv import
-        // updates x scale for new data
-        xLinearScale = xScale(disease, chosenXAxis);
-
-        // updates x axis with transition
-        xAxis = renderAxes(xLinearScale, xAxis);
-
-        // updates circles with new x values
-        circlesGroup = renderCircles(circlesGroup, xLinearScale, chosenXAxis);
-
-        // updates tooltips with new info
-        circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
-
-        // changes classes to change bold text
-        if (chosenXAxis === "num_albums") {
-          albumsLabel
-            .classed("active", true)
-            .classed("inactive", false);
-          hairLengthLabel
-            .classed("active", false)
-            .classed("inactive", true);
-        }
-        else {
-          albumsLabel
-            .classed("active", false)
-            .classed("inactive", true);
-          hairLengthLabel
-            .classed("active", true)
-            .classed("inactive", false);
-        }
-      }
-    });
 });
