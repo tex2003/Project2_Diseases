05ee2f51b27649f423a53601e9b010f07fe2a8b7
Pam Stadler <43047254+tex2003@users.noreply.github.com>
2019-03-20 13:09:27 -0500

    Added Chart 2 to app.js

--- web_app/static/js/app.js
+++ web_app/static/js/app.js
@@ -295,3 +295,53 @@ d3.json(basicURL).then(function (basicData) {
     });
 
 });
+
+//Chart 2
+
+
+// Submit Button handler
+function handleSubmit() {
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  // Select the input value from the form
+  var year = d3.select("#year").node().value;
+  console.log(year);
+
+  // clear the input value
+  d3.select("#year").node().value = "";
+
+  // Build the plot with the new year
+  buildPlot(year);
+}
+
+function buildPlot(year) {
+
+  var yearURL = "/"+year;
+  d3.json(yearURL).then(function(yearData) {
+    var disease_name = [];
+    var disease_count = [];
+    for (var i = 0; i < yearData.length; i++) { 
+      disease_name.push(yearData[i].Disease);
+      disease_count.push(yearData[i].Count);
+    };  
+     console.log(disease_count);
+      var trace1 = {
+        x: disease_name,
+        y: disease_count,
+        type: "bar"
+     };
+    
+      var data = [trace1];
+      var layout = {
+        title: "Disease Count for " + year,
+        xaxis: { title: "Diseases"},
+        yaxis: { title: "Case Count"}
+      };
+       Plotly.newPlot("plot", data,layout);
+    
+  })  
+    
+};
+  
+d3.select("#submit").on("click", handleSubmit);
